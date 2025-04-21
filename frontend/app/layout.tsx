89720b1151cd66251996027029a262fa8dea348d
Luis Daniel Mendez Castellanos <syi216618@gmail.com>
2025-04-21 16:19:54 -0700

    The name and description of the app have been changed.

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Hat App',
+  description: 'Hat Token App',
+  generator: 'Open Web Academy',
 }
 
 export default function RootLayout({
