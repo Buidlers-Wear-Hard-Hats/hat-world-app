5d5bff89e8149f357128d6148e9e92906d8b6db2
Yair Nava <iryanavahe@ittepic.edu.mx>
2025-06-09 19:13:02 -0700

    test session

--- frontend/app/claim/page.tsx
+++ frontend/app/claim/page.tsx
@@ -92,7 +92,9 @@ export default function TokenClaimPage() {
         });
 
         if (response.status === 200) {
-          setUser(MiniKit.user)
+          const user = MiniKit.user;
+          setUser(user);
+          await getHatBalance(user.walletAddress as string);
         }
         setLoading(false);
       }
@@ -101,44 +103,6 @@ export default function TokenClaimPage() {
       setLoading(false);
     }
   };
-
-  // const handleLogin = async () => {
-  //   if (!MiniKit.isInstalled()) {
-  //     return
-  //   }
-
-  //   const res = await fetch(`/api/nonce`)
-  //   const { nonce } = await res.json()
-
-  //   const { commandPayload: generateMessageResult, finalPayload } = await MiniKit.commandsAsync.walletAuth({
-  //     nonce: nonce,
-  //     requestId: '0', // Optional
-  //     expirationTime: new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000),
-  //     notBefore: new Date(new Date().getTime() - 24 * 60 * 60 * 1000),
-  //     statement: 'This is my statement and here is a link https://worldcoin.com/apps',
-  //   })
-
-  //   if (finalPayload.status === 'error') {
-  //     return
-  //   } else {
-  //     const response = await fetch('/api/complete-siwe', {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: JSON.stringify({
-  //         payload: finalPayload,
-  //         nonce,
-  //       }),
-  //     });
-  //     if (response.ok) {
-  //       const address = finalPayload.address;
-  //       localStorage.setItem('userAddress', address);
-  //       setUser({ address });
-  //       await getHatBalance(address);
-  //     }
-  //   }
-  // };
 
   const handleLogout = async () => {
     try {
@@ -248,7 +212,7 @@ export default function TokenClaimPage() {
                   <>
                     <div className="rounded-lg bg-[#FFF3A3]/60 p-4 border border-[#F9D649]">
                     {JSON.stringify(user)}
-                      <div className="text-white font-medium pb-4">Address <br /> {user?.address ? `${user.address.slice(0, 6)}...${user.address.slice(-4)}` : 'Unknown'}</div>
+                      <div className="text-white font-medium pb-4">Address <br /> {user?.walletAddress ? `${user.walletAddress.slice(0, 6)}...${user.walletAddress.slice(-4)}` : 'Unknown'}</div>
                       <div className="text-white font-medium pb-4">Balance <br /> {userBalance} HAT</div>
                       <div className="flex flex-col items-center space-y-2">
                         <Button
