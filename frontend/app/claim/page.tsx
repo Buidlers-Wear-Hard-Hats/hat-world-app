b1edbbcae68be3bfd99b173a60e4866da73d0eb6
Yair Nava <iryanavahe@ittepic.edu.mx>
2025-06-23 23:43:55 -0700

    Claim Hats

--- frontend/app/claim/page.tsx
+++ frontend/app/claim/page.tsx
@@ -22,19 +22,19 @@ import { formatUnits } from 'viem';
 import { MiniKit, WalletAuthInput } from "@worldcoin/minikit-js";
 
 const walletAuthInput = (nonce: string): WalletAuthInput => {
-    return {
-        nonce,
-        requestId: "0",
-        expirationTime: new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000),
-        notBefore: new Date(new Date().getTime() - 24 * 60 * 60 * 1000),
-        statement: "This is my statement and here is a link https://worldcoin.com/apps",
-    };
+  return {
+    nonce,
+    requestId: "0",
+    expirationTime: new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000),
+    notBefore: new Date(new Date().getTime() - 24 * 60 * 60 * 1000),
+    statement: "This is my statement and here is a link https://worldcoin.com/apps",
+  };
 };
 
 export default function TokenClaimPage() {
   const [user, setUser] = useState<any | null>(null);
   const [userBalance, setUserBalance] = useState<any | null>(null);
-  const [isValidate, setIsValidate] = useState<any | null>(false);
+  const [isValidate, setIsValidate] = useState<any | null>(0);
 
   const [debbug, setDebbug] = useState<any | null>(null);
 
@@ -45,7 +45,7 @@ export default function TokenClaimPage() {
   const isMobile = useMobile();
 
   const HAT_CONTRACT_ADDRESS = '0xbA494aEa8295B5640Efb4FF9252df8D388e655dc';
-  
+
   useEffect(() => {
     const loginUser = localStorage.getItem("userWalletAddress");
     if (loginUser) {
@@ -60,11 +60,11 @@ export default function TokenClaimPage() {
     if (!MiniKit.isInstalled()) {
       return
     }
-  
+
     setLoading(true);
     const res = await fetch(`/api/nonce`)
     const { nonce } = await res.json()
-  
+
     const { commandPayload: generateMessageResult, finalPayload } = await MiniKit.commandsAsync.walletAuth({
       nonce: nonce,
       requestId: '0', // Optional
@@ -72,7 +72,7 @@ export default function TokenClaimPage() {
       notBefore: new Date(new Date().getTime() - 24 * 60 * 60 * 1000),
       statement: 'This is my statement and here is a link https://worldcoin.com/apps',
     })
-  
+
     if (finalPayload.status === 'error') {
       setLoading(false);
       return
@@ -129,17 +129,6 @@ export default function TokenClaimPage() {
         args: [userAddress],
       });
 
-      const verifyAccount = await publicClient.readContract({
-        abi: HAT_ABI,
-        address: HAT_CONTRACT_ADDRESS,
-        functionName: "getAddressVerification",
-        args: [userAddress],
-      });
-
-      const validateAccount = verifyAccount as number > 0 ? true : false;
-
-      setIsValidate(validateAccount);
-
       const tokenAmount = Number(formatUnits(balance as bigint, 18))
 
       const formattedBalance = new Intl.NumberFormat('en-US', {
@@ -152,6 +141,26 @@ export default function TokenClaimPage() {
     } catch (error) {
       console.error("Error getting HAT balance:", error);
     }
+  }
+
+  const getAddressVerification = async () => {
+    //const loginUser = localStorage.getItem("userWalletAddress");
+    const loginUser = "0x34149390029Bbf4f4D9E7AdEa715D7055e145C05";
+    console.log("loginUser: " + loginUser);
+    if (!loginUser) return;
+
+    const publicClient = getPublicClient(config);
+
+    const verifyAccount = await publicClient.readContract({
+      abi: HAT_ABI,
+      address: HAT_CONTRACT_ADDRESS,
+      functionName: "getAddressVerification",
+      args: [loginUser],
+    });
+
+    const validateAccount = verifyAccount as number > 0 ? 2 : 1;
+
+    setIsValidate(validateAccount);
   }
 
   const getTimeToClaim = async () => {
@@ -197,7 +206,7 @@ export default function TokenClaimPage() {
 
     setDebbug(finalPayload);
 
-    if(finalPayload.status != "error"){
+    if (finalPayload.status != "error") {
       setLoading(true);
       setTimeout(() => {
         const now = Date.now();
@@ -206,7 +215,7 @@ export default function TokenClaimPage() {
         setCanClaim(false);
         setClaimed(true);
         setLoading(false);
-  
+
         setTimeout(() => {
           setClaimed(false);
           const loginUser = localStorage.getItem("userWalletAddress");
@@ -229,113 +238,67 @@ export default function TokenClaimPage() {
         backgroundRepeat: 'repeat'
       }}>
         <>
-          <Card className="w-full max-w-md bg-[#2C2C5A] text-black shadow-xl border-0 mb-1">
+          <motion.div
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8 }}
+            className="w-full max-w-md bg-[#2C2C5A] text-black shadow-xl border-0"
+            style={{ borderRadius: "10px" }}>
             <CardHeader className="text-center">
-              <CardTitle className="text-sm font-bold text-[#F5AD00] ">
-                {user ? (
-                  <>
-                    <div className="rounded-lg bg-[#FFF3A3]/60 p-4 border border-[#F9D649]">
-                      <div className="text-white font-medium pb-4"><span className="font-bold">Address</span> <br /> {user ? `${user.slice(0, 6)}...${user.slice(-4)}` : 'Unknown'}</div>
-                      <div className="text-white font-medium pb-4"><span className="font-bold">Balance</span> <br /> {userBalance} HAT</div>
-                      <div className="flex flex-col items-center space-y-2">
-                        <Button
-                          onClick={handleLogout}
-                          variant="secondary"
-                          size="default"
-                        >
-                          {"Sign Out"}
-                        </Button>
-                      </div>
-                    </div>
-                  </>
-                ) : (
-                  <>
-                    <div className="rounded-lg bg-[#FFF3A3]/60 p-4 border border-[#F9D649]">
-                      <p className="mt-2 text-sm text-white">
-                        Please login to request HAT tokens.
-                      </p>
-                      <div className="flex flex-col items-center space-y-2 w-full mt-2">
-                        <Button
-                          onClick={handleLogin}
-                        >
-                          Sign In
-                        </Button>
-                      </div>
-                    </div>
-                  </>
-                )}
-              </CardTitle>
-            </CardHeader>
-          </Card>
-          {process.env.NEXT_PUBLIC_APP_STATE == "placeholder" ? (
-
-            <Card className="w-full max-w-md bg-[#2C2C5A] text-black shadow-xl border-0">
-              <CardHeader className="text-center">
-
+              <div className="mx-auto mb-4 flex h-24 w-24 items-center justify-center rounded-full bg-[#F9D649] border-2 border-black">
                 <img
                   src="https://hat.ow.academy/assets/icon.png"
                   alt="HAT Icon"
-                  className="h-64 object-contain"
+                  className="h-24 object-contain"
                 />
-                <CardTitle className="text-4xl font-bold py-8 text-[#F5AD00] ">
-                  HAT Token
-                </CardTitle>
-              </CardHeader>
-              <CardContent className="text-center">
-                <p className="text-white text-4xl pb-8 italic font-bold">Coming Soon</p>
-              </CardContent>
-            </Card>
-          ) : (
-            <motion.div
-              initial={{ opacity: 0, y: -20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.8 }}
-              className="w-full max-w-md bg-[#2C2C5A] text-black shadow-xl border-0"
-              style={{ borderRadius: "10px" }}>
-              <CardHeader className="text-center">
-                <div className="mx-auto mb-4 flex h-24 w-24 items-center justify-center rounded-full bg-[#F9D649] border-2 border-black">
-                  <img
-                    src="https://hat.ow.academy/assets/icon.png"
-                    alt="HAT Icon"
-                    className="h-24 object-contain"
+              </div>
+              <CardTitle className="text-2xl font-bold text-[#F5AD00] ">
+                Claim HAT Tokens
+              </CardTitle>
+              <CardDescription className="text-white">
+                Claim your daily HAT tokens
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="text-center">
+              {!canClaim && lastClaim && (
+                <div className="mb-6">
+                  <p className="mb-2 text-[#F5AD00]">Time left to claim:</p>
+                  <Countdown
+                    targetDate={lastClaim + 24 * 60 * 60 * 1000}
+                    onComplete={handleCooldownComplete}
                   />
                 </div>
-                <CardTitle className="text-2xl font-bold text-[#F5AD00] ">
-                  Claim HAT Tokens
-                </CardTitle>
-                <CardDescription className="text-white">
-                  Claim your daily HAT tokens
-                </CardDescription>
-              </CardHeader>
-              <CardContent className="text-center">
-                {!canClaim && lastClaim && (
-                  <div className="mb-6">
-                    <p className="mb-2 text-[#F5AD00]">Time left to claim:</p>
-                    <Countdown
-                      targetDate={lastClaim + 24 * 60 * 60 * 1000}
-                      onComplete={handleCooldownComplete}
-                    />
-                  </div>
-                )}
+              )}
 
-                {claimed && (
-                  <div className="mb-4 rounded-md bg-[#F9D649]/30 p-3 text-[#2C2C5A] font-bold border border-[#F9D649]">
-                    HAT tokens claimed successfully!
-                  </div>
-                )}
-
-                <div className="mt-4 rounded-lg bg-[#FFF3A3]/60 p-4 border border-[#F9D649]">
-                  <p className="text-lg text-black font-bold">Information:</p>
-                  <p className="mt-2 text-sm text-white">
-                    You can claim HAT tokens once every 24 hours. The tokens will be
-                    automatically sent to your connected wallet.
-                  </p>
+              {claimed && (
+                <div className="mb-4 rounded-md bg-[#F9D649]/30 p-3 text-[#2C2C5A] font-bold border border-[#F9D649]">
+                  HAT tokens claimed successfully!
                 </div>
-              </CardContent>
+              )}
+
+              <div className="mt-4 rounded-lg bg-[#FFF3A3]/60 p-4 border border-[#F9D649]">
+                <p className="text-lg text-black font-bold">Information:</p>
+                <p className="mt-2 text-sm text-white">
+                  You can claim HAT tokens once every 24 hours. The tokens will be
+                  automatically sent to your connected wallet.
+                </p>
+              </div>
+            </CardContent>
+            {!user ?
               <CardFooter>
-                {user && (
+                <div className="flex flex-col items-center space-y-2 w-full mt-2">
+                  <Button
+                    onClick={handleLogin}
+                  >
+                    Sign In
+                  </Button>
+                </div>
+              </CardFooter>
+              :
+              <CardFooter>
+                {!user && (
                   <>
-                    {isValidate ?
+                    {isValidate == 2 ?
                       <Button
                         className={`w-full ${canClaim
                           ? "bg-[#F9D649] hover:bg-[#FFE066] text-black"
@@ -352,19 +315,28 @@ export default function TokenClaimPage() {
                             : "On Cooldown"}
                       </Button>
                       :
-                      <Button
-                        className={`w-full bg-[#F9D649] hover:bg-[#FFE066] text-black`}
-                        disabled={!isValidate}
-                        size={isMobile ? "lg" : "default"}
-                      >
-                        You need verify your World account to claim HAT
-                      </Button>
+                      isValidate == 1 ?
+                        <Button
+                          className={`w-full bg-[#F9D649] hover:bg-[#FFE066] text-black`}
+                          disabled={isValidate == 1}
+                          size={isMobile ? "lg" : "default"}
+                        >
+                          You need verify your World Id to claim HAT
+                        </Button>
+                        :
+                        <Button
+                          className={`w-full bg-[#F9D649] hover:bg-[#FFE066] text-black`}
+                          onClick={getAddressVerification}
+                          size={isMobile ? "lg" : "default"}
+                        >
+                          Verify your account
+                        </Button>
                     }
                   </>
                 )}
               </CardFooter>
-            </motion.div>
-          )}
+            }
+          </motion.div>
         </>
       </main>
     </>
